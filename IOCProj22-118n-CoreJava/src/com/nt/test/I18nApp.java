7e0d7bb7dd74e3391583cc037d208ece4dd2cb5f
hp <hp@venkat>
2021-05-30 22:01:58 +0530

    I18n

---
+++ IOCProj22-118n-CoreJava/src/com/nt/test/I18nApp.java
@@ -0,0 +1,18 @@
+package com.nt.test;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class I18nApp {
+
+	public static void main(String[] args) {
+
+		// create Locale object
+		Locale locale = new Locale(args[0], args[1]);
+		// create ResourceBundle object
+		ResourceBundle bundle = ResourceBundle.getBundle("com/nt/commons/App", locale);
+		System.out.println(bundle.getString("wishMsg") + "    " + bundle.getString("welcome.msg") + "   "
+				+ bundle.getString("bye.msg"));
+	}
+
+}
---
+++ IOCProj23-SpringI18n-ResourceBundleMessageSource/src/main/java/com/nt/test/SpringI18nTest.java
@@ -0,0 +1,31 @@
+package com.nt.test;
+
+import java.util.Date;
+import java.util.Locale;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringI18nTest {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext ctx=new ClassPathXmlApplicationContext("com/nt/cfgs/applicationContext.xml");
+
+		Locale locale=new Locale(args[0], args[1]);
+	
+		String msg=ctx.getMessage("wishMsg1"+"{0}", new Object[] {"buddy",new Date()}, "No data found:: "+"{0}"+" at -->"+"{1,time}", locale);
+
+		System.out.println(msg);
+		
+		String msg1=ctx.getMessage("wishMsg", null, locale);
+		String msg2=ctx.getMessage("welcome.msg", null, locale);
+		String msg3=ctx.getMessage("bye.msg", null, locale);
+		
+		System.out.println(msg1+"   "+msg2+"   "+msg3);
+		
+		((AbstractApplicationContext) ctx).close();
+	}
+
+}
